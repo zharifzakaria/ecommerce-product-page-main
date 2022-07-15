cf8d2ffe8c31b6c8536f9cd1440108ed6c6c0fbf
zharifzakaria <zharifzakaria@gmail.com>
2022-07-15 15:34:55 +0800

    cart checkout function added

--- js/script.js
+++ js/script.js
@@ -81,3 +81,89 @@ plus.addEventListener('click', () => {
   currentCounter++;
   counterDisplay.innerHTML = currentCounter;
 })
+
+//handle add cart
+const cartTableRow = document.getElementById('cartTable').querySelector('.row');
+let price = 125.00;
+let total = 0;
+
+function handleAddCart() {
+  total = currentCounter;
+  filledCart();
+  if(total == 0) emptyCart();
+
+  const cartAmount = document.getElementById('cartAmount');
+  cartAmount.innerHTML = total;
+  if(total !== 0) {
+    cartAmount.style.backgroundColor = 'hsl(26, 100%, 55%)'
+  } else cartAmount.style.backgroundColor = 'hsl(220, 14%, 75%)';
+}
+
+function emptyCart() {
+  total = 0;
+  currentCounter = 0;
+
+  cartTableRow.innerHTML = '';
+  const p = document.createElement('p');
+  p.classList.add('empty-cart');
+  p.innerHTML = 'Your cart is empty.';
+  cartTableRow.appendChild(p);
+  cartTableRow.style.justifyContent = 'center';
+
+  cartAmount.innerHTML = total;
+  counterDisplay.innerHTML = total;
+  cartAmount.style.backgroundColor = 'hsl(220, 14%, 75%)';
+
+  const checkoutBtn = document.getElementById('checkoutBtn');
+  checkoutBtn.style.display = 'none';
+  document.getElementById('checkoutBtn').querySelector('button').innerHTML = "Checkout";
+}
+
+emptyCart();
+
+function filledCart() {
+  checkoutBtn.style.display = 'block';
+  const thumb = document.createElement('div');
+  thumb.classList.add('thumb');
+  const thumbImg = document.createElement('img')
+  thumbImg.src = document.getElementById("mainImage").src;
+  thumb.appendChild(thumbImg);
+
+  const product = document.createElement('div');
+  product.classList.add('product');
+
+  const productName = document.createElement('div');
+  productName.classList.add('product__name');
+  productName.innerHTML = 'Fall Limited Edition Sneakers'
+  product.appendChild(productName);
+  
+  const productPrice = document.createElement('div');
+  productPrice.classList.add('product__price');
+  productPrice.innerHTML = price + ' x ' + total + '<strong>' + formatter.format(price*total) +'</strong>'
+  product.appendChild(productPrice);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.setAttribute('onclick','emptyCart()')
+  const deleteBtnIcon = document.createElement('img');
+  deleteBtnIcon.src = './images/icon-delete.svg';
+  deleteBtn.appendChild(deleteBtnIcon);
+
+  while (cartTableRow.firstChild) {
+    cartTableRow.removeChild(cartTableRow.firstChild);
+  }
+  cartTableRow.appendChild(thumb);
+  cartTableRow.appendChild(product);
+  cartTableRow.appendChild(deleteBtn);
+}
+
+// currency formatter
+var formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+function handleCheckOut() {
+  const checkoutBtn = document.getElementById('checkoutBtn').querySelector('button');
+  checkoutBtn.innerHTML = "Paid! Thank Youzzz";
+}
